368de278909e2082e2a6ec4bafb0b9925c39c2c1
David Stewart <davidstewy@gmail.com>
2018-05-10 14:05:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const gotCitiesCSV = "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
+
+const lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
+
+const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
+
+function katas1(){
+    holdArray = document.createElement("p")
+    holdArray.textContent = JSON.stringify(lotrCitiesArray);
+}
+    katas1();
